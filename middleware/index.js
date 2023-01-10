7f4c511ac4f0f662412116d167634bdbe3cd16e2
heri <herinug2005@gmail.com>
2023-01-10 08:56:16 +0700

    istal jwt

---
+++ middleware/index.js
@@ -0,0 +1,7 @@
+var express = require ('express');
+var auth = require ('./auth');
+var router = express.Router();
+
+router.post('/api/vi/register', auth.registrasi);
+router.post('/api/vi/login', auth.login);
+module.exports= router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const  express = require('express');
 const bodyParser = require ('body-parser');
+var morgan = require('morgan');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
+// panggil route
 var routes = require('./routes');
 routes(app);
+// daftarkan menu routes
+app.use('/auth',require('./middleware'));
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
